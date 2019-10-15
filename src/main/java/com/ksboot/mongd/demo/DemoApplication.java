e3d745c5a620173804cc027b95b8541235e32617
chizengkun <chizengkun@kingsoft.com>
2019-10-15 09:26:23 +0800

    Initial commit

---
+++ src/main/java/com/ksboot/mongd/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ksboot.mongd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
