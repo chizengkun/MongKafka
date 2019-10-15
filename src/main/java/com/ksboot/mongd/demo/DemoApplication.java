1fe033cd2c3ddc9b6235e52af40a2fcbe1420176
chizengkun <chizengkun@kingsoft.com>
2019-10-15 12:12:21 +0800

    +update code

--- src/main/java/com/ksboot/mongd/demo/DemoApplication.java
+++ src/main/java/com/ksboot/mongd/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.ksboot.mongd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ksboot/mongd/demo/configuration/KafkaAutoSendSvr.java
@@ -0,0 +1,26 @@
+package com.ksboot.mongd.demo.configuration;
+
+import com.ksboot.mongd.demo.service.KafkaSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaAutoSendSvr implements ApplicationListener<ApplicationStartedEvent> {
+    @Autowired
+    private KafkaSender kafkaSender;
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        for (int i = 0; i < 5; i++) {
+            //调用消息发送类中的消息发送方法
+            kafkaSender.send();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
